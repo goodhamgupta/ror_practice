7f3dda823bf34ee5bfbfd9bda0ec899c59e61bc3
shubhamgupta <shubham.gupta@scripbox.com>
2016-11-04 18:44:24 +0530

    RLE partially complete.

--- ruby_practice/exercism_questions/ruby/run-length-encoding/run_length_encoding.rb
+++ ruby_practice/exercism_questions/ruby/run-length-encoding/run_length_encoding.rb
@@ -1,7 +1,32 @@
 # Program to perform RLE
 
+module BookKeeping
+  VERSION = 2
+end
 class RunLengthEncoding
   def self.encode(string)
-    
+    count = 0
+    result = ""
+    string.split("").each_with_index do |letter, char_index|
+      count += 1
+      if string[char_index+1] != letter
+        if count == 1
+          result.concat(letter)
+        else
+          result.concat(count.to_s + letter)
+        end
+        count = 0
+      end
+    end
+    result
+  end
+
+  def self.decode(string)
+    result = ""
+    string.split("").each_cons(2) do |count, letter|
+      result.concat(letter * count.to_i)
+    end
+    result
   end
 end
+
