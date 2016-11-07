87623a5966328d8a3edf669b525df2b7711067a7
shubhamgupta <shubham.gupta@scripbox.com>
2016-11-07 16:35:58 +0530

    Add user signup route and page

--- rails5/sample_app/config/routes.rb
+++ rails5/sample_app/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  # Changing users new to users signup url
+  get '/signup', to: 'users#new'
 end
