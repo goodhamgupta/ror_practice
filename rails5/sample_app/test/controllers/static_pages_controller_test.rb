0513c96d6e6162b969b4be20c89af455073f707c
shubhamgupta <shubham.gupta@scripbox.com>
2016-11-07 16:29:26 +0530

    Add integration tests for layout. Complete custom stylesheet using scss

--- rails5/sample_app/test/controllers/static_pages_controller_test.rb
+++ rails5/sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,33 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get root" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should fetch about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should fetch Contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
-
 end
