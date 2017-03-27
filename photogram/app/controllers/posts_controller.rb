1008851efe7bdd7240e7ed392ec1a5ab7608168b
Shubham Gupta <shubhamg22>
2017-03-28 00:16:59 +0530

    CRUD app complete

--- photogram/app/controllers/posts_controller.rb
+++ photogram/app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -9,12 +11,45 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(post_params)
-    redirect_to new_post_path
+    if post
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "You dumb piece of shit!"
+      redirect_to new_post_path
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Your post has been updated!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "You dumb piece of shit again!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to post_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:caption, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- photogram/config/application.rb
+++ photogram/config/application.rb
@@ -13,3 +13,11 @@ module Photogram
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+def alert_for(flash_type)
+  { success: 'alert-success',
+    error: 'alert-danger',
+    alert: 'alert-warning',
+    notice: 'alert-info'
+  }[flash_type.to_sym] || flash_type.to_s
+end
